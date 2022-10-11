c721efae11501128e4b89eb1e2ccfb1636d91984
Reyhan Karamahmut <reyhan.karamahmut@icloud.com>
2022-10-11 19:35:58 +0200

    feat: implement removing pieces, mill check

--- src/main/scala/de/htwg/se/mill/model/Game.scala
+++ src/main/scala/de/htwg/se/mill/model/Game.scala
@@ -5,20 +5,24 @@ import de.htwg.se.mill.model.Player
 import de.htwg.se.mill.model.Field
 
 /*
-      a             b             c
-    1 ⚫――――――――――――⚫――――――――――――⚫
-      │   ⚫――――――――⚫――――――――⚫   │ 1
-      │   │   ⚫――――⚫――――⚫   │ 2   │
-      │   │   │            │ 3  │   │
-    2 ⚫――⚫――⚫          ⚫――⚫――⚫
-      │   │   │            │   │   │
-      │   │   ⚫――――⚫――――⚫   │   │
-      │   ⚫――――――――⚫――――――――⚫   │
-    3 ⚫――――――――――――⚫――――――――――――⚫
+  a             b             c
+1 ⚫――――――――――――⚫――――――――――――⚫
+  │   ⚫――――――――⚫――――――――⚫   │ 1
+  │   │   ⚫――――⚫――――⚫   │ 2   │
+  │   │   │            │ 3  │   │
+2 ⚫――⚫――⚫          ⚫――⚫――⚫
+  │   │   │            │   │   │
+  │   │   ⚫――――⚫――――⚫   │   │
+  │   ⚫――――――――⚫――――――――⚫   │
+3 ⚫――――――――――――⚫――――――――――――⚫
  */
-// chess input notation: a11 a21, a11, a11 c33
-enum GameState {
-  case Setting, Moving, Removing, Flying, Finish
+enum GameState(value: String) {
+  def representation = value
+  case Setting extends GameState("Setting pieces")
+  case Moving extends GameState("Moving pieces")
+  case Removing extends GameState("Removing piece")
+  case Flying extends GameState("Flying pieces")
+  case Won extends GameState("Won")
 }
 final case class Game(
     board: Board,
@@ -37,9 +41,27 @@ final case class Game(
     Math.abs(from.x - to.x) == 1 ^ Math.abs(from.y - to.y) == 1
     ^ Math.abs(from.ring - to.ring) == 1
 
-  // TODO: Ausgehend von to, eckpunkte entscheiden, dimensionen fixieren und alle auf dem weg
-  // liegenden steine nach farbe zaehlen
-  def isMill(to: Field, board: Board): Boolean = true
+  def isMill(to: Field, board: Board): Boolean = {
+    // TODO: check
+    val possibleMillOnRow = board.fields
+      .count(field =>
+        field.y == to.y && field.ring == to.ring && field.color == to.color
+      ) == board.size
+    val possibleMillOnColumn = board.fields
+      .count(field =>
+        field.x == to.x && field.ring == to.ring && field.color == to.color
+      ) == board.size
+    val isMiddlePoint =
+      to.x == Math.floor(board.size / 2) || to.y == Math.floor(board.size / 2)
+    if (isMiddlePoint) {
+      val possibleMillOnRing = board.fields
+        .count(field =>
+          field.y == to.y && field.x == to.x && field.color == to.color
+        ) == board.size
+      possibleMillOnRow || possibleMillOnColumn || possibleMillOnRing
+    }
+    possibleMillOnColumn || possibleMillOnRow
+  }
 
   @throws(classOf[IllegalArgumentException])
   def setPiece(player: Player, field: Field): Game = {
@@ -60,7 +82,8 @@ final case class Game(
       board.size
     )
     val playedTurnState =
-      if (
+      if (isMill(field, board)) GameState.Removing
+      else if (
         playedTurnBoard.fields
           .count(field => field.color != UnsetFieldColor) < Math
           .pow(playedTurnBoard.size, 2)
@@ -69,10 +92,16 @@ final case class Game(
       else GameState.Moving
     Game(playedTurnBoard, players, playedTurnState)
   }
+  @throws(classOf[IllegalArgumentException])
   def movePiece(player: Player, from: Field, to: Field): Game = {
     if (state != GameState.Moving && state != GameState.Flying) {
       throw new IllegalArgumentException(
         "The piece was not moved. Please provide a valid input for moving or flying a piece."
+      )
+    }
+    if (from.color != player.color) {
+      throw new IllegalArgumentException(
+        "The piece was not moved. You can only move your own pieces."
       )
     }
     if (state == GameState.Moving) {
@@ -99,20 +128,38 @@ final case class Game(
       board.size
     )
     val playedTurnState =
-      if (
-        playedTurnBoard.fields
-          .count(field => field.color != UnsetFieldColor) < Math
-          .pow(playedTurnBoard.size, 2)
-          .toInt * 2
-      ) state
-      else GameState.Moving
+      if (isMill(to, board)) GameState.Removing
+      else state
     Game(board, players, state)
   }
+  @throws(classOf[IllegalArgumentException])
   def removePiece(player: Player, field: Field): Game = {
     if (state != GameState.Removing) {
       throw new IllegalArgumentException(
         "The piece was not removed. Please provide a valid input for removing a piece."
       )
+    }
+    if (field.color == player.color) {
+      throw new IllegalArgumentException(
+        "The piece was not removed. You cannot remove your own pieces."
+      )
+    }
+    val playedTurnBoard = Board(
+      board.fields
+        .updated(
+          board.fields.indexOf(field),
+          new Field(field, UnsetFieldColor)
+        ),
+      board.size
+    )
+    var playedTurnState = GameState.Moving
+    val otherPlayersPieces = playedTurnBoard.fields.count(field =>
+      field.color == players.find(p => !p.equals(player)).get.color
+    )
+    if (otherPlayersPieces < 3) {
+      playedTurnState = GameState.Won
+    } else if (otherPlayersPieces == 3) {
+      playedTurnState = GameState.Flying
     }
     Game(board, players, state)
   }
