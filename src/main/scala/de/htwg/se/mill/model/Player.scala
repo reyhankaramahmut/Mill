38c12fb7b39c790b8530f6b37ebbb494f10bee31
Reyhan Karamahmut <reyhan.karamahmut@icloud.com>
2022-10-11 19:33:59 +0200

    feat: add equality check for players

--- src/main/scala/de/htwg/se/mill/model/Player.scala
+++ src/main/scala/de/htwg/se/mill/model/Player.scala
@@ -2,4 +2,9 @@ package de.htwg.se.mill.model
 
 case class Player(name: String, color: String) {
   override def toString: String = s"$name $color"
+  override def equals(player: Any): Boolean = player match {
+    case p: Player =>
+      p.color.equals(color) && p.name.equals(name)
+    case _ => false
+  }
 }
