679a733839470cab30b46eed5bb2cce2c11c54a1
Melanie Galip <melanie.galip@outlook.de>
2022-10-11 18:38:35 +0200

    feat: override field equals

--- src/main/scala/de/htwg/se/mill/model/Field.scala
+++ src/main/scala/de/htwg/se/mill/model/Field.scala
@@ -7,7 +7,6 @@ case class Field(x: Int, y: Int, ring: Int, color: String = UnsetFieldColor) {
     this(field.x, field.y, field.ring, color)
 
   override def toString: String = color
-
   override def equals(field: Any): Boolean = field match {
     case f: Field =>
       f.x.equals(x) && f.y.equals(y) && f.ring.equals(ring)
