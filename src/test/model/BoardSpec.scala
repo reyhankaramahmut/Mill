be221a187306c366664aa33ef5be9d75a33dce85
Reyhan Karamahmut <reyhan.karamahmut@icloud.com>
2022-10-11 19:24:59 +0200

    feat: tests

---
+++ src/test/model/BoardSpec.scala
@@ -0,0 +1,7 @@
+package de.htwg.se.mill.model
+
+import de.htwg.se.mill.model.Board
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class BoardSpec extends AnyWordSpec with Matchers {}
