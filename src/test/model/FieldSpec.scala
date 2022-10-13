d627c545b2e1d2967a2104acfa5f15219fed0c49
Melanie Galip <melanie.galip@outlook.de>
2022-10-13 19:42:46 +0200

    test: field spec

--- src/test/model/FieldSpec.scala
+++ src/test/model/FieldSpec.scala
@@ -6,6 +6,21 @@ import org.scalatest.wordspec.AnyWordSpec
 
 class FieldSpec extends AnyWordSpec with Matchers {
   "A Field" when {
+    "auxiliary new" should {
+      val field = new Field(Field(0, 0, 0), "🔵")
+      "have a x" in {
+        field.x should be(0)
+      }
+      "have a y" in {
+        field.y should be(0)
+      }
+      "have a ring" in {
+        field.ring should be(0)
+      }
+      "have a player color" in {
+        field.color should be("🔵")
+      }
+    }
     "new" should {
       val field = Field(0, 0, 0, "🔴")
       "have a x" in {
@@ -22,6 +37,29 @@ class FieldSpec extends AnyWordSpec with Matchers {
       }
       "have a nice String representation" in {
         field.toString should be("🔴")
+      }
+      "have a unset field color" in {
+        field.unsetFieldColor should be("⚫")
+      }
+      "equal a field with the same coordinate and ring" in {
+        val sameField = Field(0, 0, 0, "🔴")
+        field should equal(sameField)
+      }
+      "not equal a field with a different x coordinate" in {
+        val differentField = Field(1, 0, 0, "🔴")
+        field should not equal (differentField)
+      }
+      "not equal a field with a different y coordinate" in {
+        val differentField = Field(0, 1, 0, "🔴")
+        field should not equal (differentField)
+      }
+      "not equal a field with a different ring" in {
+        val differentField = Field(0, 0, 1, "🔴")
+        field should not equal (differentField)
+      }
+      "not equal a field with a different type" in {
+        val differentField = ""
+        field should not equal (differentField)
       }
     }
   }
