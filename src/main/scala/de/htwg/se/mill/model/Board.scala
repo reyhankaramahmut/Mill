dce4ea960b1ae2f036eb8d0662ed4bd975050a31
Melanie Galip <melanie.galip@outlook.de>
2022-10-13 20:29:19 +0200

    feat: factory pattern & try monad

--- src/main/scala/de/htwg/se/mill/model/Board.scala
+++ src/main/scala/de/htwg/se/mill/model/Board.scala
@@ -1,77 +1,89 @@
 package de.htwg.se.mill.model
-import de.htwg.se.mill.model.Field
+import scala.util.{Try, Success, Failure}
 import java.lang.IllegalArgumentException
 import scala.collection.immutable.ListMap
 
-case class Board(fields: List[Field], size: Int) {
-  val endOfLine = sys.props("line.separator")
-  val lineHeight = 1
-  val barWidth = 4
-  val spaceWidth = 3
-  @throws(classOf[IllegalArgumentException])
-  // auxiliary constructor that creates the initial board with its fields respectively
-  def this(size: Int = 3) = this(
+trait Board {
+  def fields: List[Field]
+  def size: Int
+}
+
+object Board {
+  private case class NewBoard(fields: List[Field], size: Int) extends Board {
+    val endOfLine = sys.props("line.separator")
+    val lineHeight = 1
+    val barWidth = 4
+    val spaceWidth = 3
+    override def toString(): String = {
+      def bar(width: Int = barWidth) = "―" * width
+      def line(height: Int = lineHeight) = "│" * height
+      def space(width: Int = spaceWidth) = " " * width
+      def dividerRow =
+        (line() + space()) * size + space() * (size - 1) + (space() + line()) * size
+          + endOfLine
+      def formattedFieldsByRing = (fieldsByRing: (Int, List[Field])) =>
+        (line() + space()) * fieldsByRing(0) + fieldsByRing(1)
+          .mkString(bar((((size - 1) - fieldsByRing(0)) + 1) * barWidth))
+          + (space() + line()) * fieldsByRing(0)
+      val middleSection =
+        fields
+          .filter(field => field.y > 0 && field.y < (size - 1))
+      val middleSections = middleSection.splitAt(middleSection.length / 2)
+
+      val upperSection = fields
+        .filter(field => field.y == 0)
+        .groupBy(_.ring)
+        .map(formattedFieldsByRing)
+        .mkString(endOfLine)
+        + endOfLine
+      val lowerSection = ListMap(
+        fields
+          .filter(field => field.y == (size - 1))
+          .groupBy(_.ring)
+          .toSeq
+          .sortWith(_._1 > _._1): _*
+      )
+        .map(formattedFieldsByRing)
+        .mkString(endOfLine)
+
+      endOfLine
+        + upperSection
+        + dividerRow
+        + middleSections(0).mkString(
+          bar(barWidth / 2)
+        ) + space() * size + middleSections(1).mkString(
+          bar(barWidth / 2)
+        ) + endOfLine
+        + dividerRow
+        + lowerSection
+    }
+    def fieldsDump = fields
+      .map(field => s"(${field.x}, ${field.y}, ${field.ring})")
+      .mkString(",")
+  }
+  def withSize(size: Int = 3): Try[Board] = {
     // check if board size is valid
     if (size < 3 || size % 2 == 0 || size > 9)
-      throw new IllegalArgumentException(
-        "Invalid board size. Should be uneven and greater than 2."
+      Failure(
+        IllegalArgumentException(
+          "Invalid board size. Should be uneven and greater than 2."
+        )
       )
-    // construct initial fields
-    else
-      (for {
-        ring <- 0 until size
-        row <- 0 until size
-        col <- 0 until size;
-        // filter out inner fields
-        if !(row > 0 && row < size - 1 && col > 0 && col < size - 1)
-      } yield Field(col, row, ring)).toList,
-    size
-  )
-
-  override def toString(): String = {
-    def bar(width: Int = barWidth) = "―" * width
-    def line(height: Int = lineHeight) = "│" * height
-    def space(width: Int = spaceWidth) = " " * width
-    def dividerRow =
-      (line() + space()) * size + space() * (size - 1) + (space() + line()) * size
-        + endOfLine
-    def formattedFieldsByRing = (fieldsByRing: (Int, List[Field])) =>
-      (line() + space()) * fieldsByRing(0) + fieldsByRing(1)
-        .mkString(bar((((size - 1) - fieldsByRing(0)) + 1) * barWidth))
-        + (space() + line()) * fieldsByRing(0)
-    val middleSection =
-      fields
-        .filter(field => field.y > 0 && field.y < (size - 1))
-    val middleSections = middleSection.splitAt(middleSection.length / 2)
-
-    val upperSection = fields
-      .filter(field => field.y == 0)
-      .groupBy(_.ring)
-      .map(formattedFieldsByRing)
-      .mkString(endOfLine)
-      + endOfLine
-    val lowerSection = ListMap(
-      fields
-        .filter(field => field.y == (size - 1))
-        .groupBy(_.ring)
-        .toSeq
-        .sortWith(_._1 > _._1): _*
+    Success(
+      NewBoard(
+        // construct initial fields
+        (for {
+          ring <- 0 until size
+          row <- 0 until size
+          col <- 0 until size;
+          // filter out inner fields
+          if !(row > 0 && row < size - 1 && col > 0 && col < size - 1)
+        } yield Field(col, row, ring)).toList,
+        size
+      )
     )
-      .map(formattedFieldsByRing)
-      .mkString(endOfLine)
-
-    endOfLine
-      + upperSection
-      + dividerRow
-      + middleSections(0).mkString(
-        bar(barWidth / 2)
-      ) + space() * size + middleSections(1).mkString(
-        bar(barWidth / 2)
-      ) + endOfLine
-      + dividerRow
-      + lowerSection
   }
-  def fieldsDump = fields
-    .map(field => s"(${field.x}, ${field.y}, ${field.ring})")
-    .mkString(",")
+  def apply(fields: List[Field], size: Int): Board = {
+    NewBoard(fields, size)
+  }
 }
