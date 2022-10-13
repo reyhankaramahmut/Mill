767ae69b22d8bb8257935a2599903e7d22af61bc
Melanie Galip <melanie.galip@outlook.de>
2022-10-13 19:40:21 +0200

    feat: refactor game state

--- src/main/scala/de/htwg/se/mill/model/Game.scala
+++ src/main/scala/de/htwg/se/mill/model/Game.scala
@@ -16,14 +16,6 @@ import de.htwg.se.mill.model.Field
       │   ⚫――――――――⚫――――――――⚫   │
     3 ⚫――――――――――――⚫――――――――――――⚫
  */
-enum GameState(value: String) {
-  def representation = value
-  case Setting extends GameState("Setting pieces")
-  case Moving extends GameState("Moving pieces")
-  case Removing extends GameState("Removing piece")
-  case Flying extends GameState("Flying pieces")
-  case Won extends GameState("Won")
-}
 final case class Game(
     board: Board,
     players: Vector[Player],
