8e11378215764fad4f5b6d6009561209c9d8924b
Reyhan Karamahmut <reyhan.karamahmut@icloud.com>
2022-10-25 14:29:38 +0200

    feat: state pattern + refactoring

--- src/main/scala/de/htwg/se/mill/model/GameState.scala
+++ src/main/scala/de/htwg/se/mill/model/GameState.scala
@@ -1,10 +1,222 @@
 package de.htwg.se.mill.model
 
-enum GameState(value: String) {
-  def representation = value
-  case Setting extends GameState("Setting pieces")
-  case Moving extends GameState("Moving pieces")
-  case Removing extends GameState("Removing piece")
-  case Flying extends GameState("Flying pieces")
-  case Won extends GameState("Won")
+import scala.util.{Try, Success, Failure}
+import de.htwg.se.mill.util.Messages
+
+sealed trait GameState(val game: Game) {
+  override def equals(state: Any): Boolean = state match {
+    case s: GameState => s.game.equals(game)
+    case _            => false
+  }
+  def nextState(game: Game, fields: (Field, Option[Field])): GameState
+  def execute(fields: (Field, Option[Field])): Try[GameState]
+  def handle(
+      e: GameEvent,
+      fields: (Field, Option[Field])
+  ): Try[GameState] = {
+    e match {
+      case GameEvent.OnSetting =>
+        if (!this.isInstanceOf[SettingState])
+          return Failure(
+            IllegalArgumentException(
+              Messages.isNotSettingStateMessage
+            )
+          )
+
+      case GameEvent.OnMoving =>
+        if (!this.isInstanceOf[MovingState])
+          return Failure(
+            IllegalArgumentException(
+              Messages.isNotMovingStateMessage
+            )
+          )
+
+      case GameEvent.OnFlying =>
+        if (!this.isInstanceOf[FlyingState])
+          return Failure(
+            IllegalArgumentException(
+              Messages.isNotFlyingStateMessage
+            )
+          )
+      case GameEvent.OnRemoving =>
+        if (!this.isInstanceOf[RemovingState])
+          return Failure(
+            IllegalArgumentException(
+              Messages.isNotRemovingStateMessage
+            )
+          )
+
+    }
+    execute(fields)
+  }
+}
+
+private trait Moving(game: Game) {
+  def movePiece(fields: (Field, Option[Field])): Try[Game] = {
+    if (fields(0).color != game.currentPlayer.color)
+      return Failure(
+        IllegalArgumentException(
+          Messages.movedOtherPieceMessage
+        )
+      )
+
+    Success(
+      game.copy(board =
+        Board(
+          game.board.fields
+            .updated(
+              game.board.fields.indexOf(fields(0)),
+              fields(0).copy(color = fields(0).unsetFieldColor)
+            )
+            .updated(
+              game.board.fields.indexOf(fields(1).get),
+              fields(1).get.copy(color = game.currentPlayer.color)
+            ),
+          game.board.size
+        )
+      )
+    )
+  }
+}
+
+case class MovingState(override val game: Game)
+    extends GameState(game)
+    with Moving(game) {
+  override def nextState(
+      game: Game,
+      fields: (Field, Option[Field])
+  ): GameState =
+    if (game.isMill(fields(1).get)) RemovingState(game)
+    else copy(game)
+  override def execute(fields: (Field, Option[Field])): Try[GameState] = {
+    if (!game.isValidMove(fields(0), fields(1).get))
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidMoveFieldAlreadyInUseMessage
+        )
+      )
+
+    movePiece(fields).map(game => nextState(game, fields))
+  }
+}
+case class SettingState(override val game: Game) extends GameState(game) {
+  override def nextState(
+      game: Game,
+      fields: (Field, Option[Field])
+  ): GameState =
+    if (game.isMill(fields(0))) RemovingState(game)
+    else if (
+      game.copy(setStones = game.setStones + 1).everyPlayerHasSetItsStones
+    ) MovingState(game)
+    else copy(game)
+
+  override def execute(fields: (Field, Option[Field])): Try[GameState] = {
+    if (!game.isValidSet(fields(0)))
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidSetFieldAlreadyInUseMessage
+        )
+      )
+    Success(
+      nextState(
+        game.copy(
+          board = Board(
+            game.board.fields.updated(
+              game.board.fields.indexOf(fields(0)),
+              fields(0).copy(color = game.currentPlayer.color)
+            ),
+            game.board.size
+          )
+        ),
+        fields
+      )
+    )
+  }
+}
+case class RemovingState(
+    override val game: Game
+) extends GameState(game) {
+  override def nextState(
+      game: Game,
+      fields: (Field, Option[Field])
+  ): GameState = {
+    var nextState: GameState = SettingState(game)
+    if (game.everyPlayerHasSetItsStones) {
+      nextState = MovingState(game)
+      val otherPlayersPieces = game.board.fields.count(field =>
+        field.color == game.players
+          .find(p => !p.equals(game.currentPlayer))
+          .get
+          .color
+      )
+      if (otherPlayersPieces == game.board.size) {
+        nextState = FlyingState(game)
+      }
+    }
+    nextState
+  }
+  override def execute(fields: (Field, Option[Field])): Try[GameState] = {
+    if (fields(0).color == game.currentPlayer.color)
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidRemoveFieldOwnPieceMessage
+        )
+      )
+
+    if (fields(0).color == fields(0).unsetFieldColor)
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidRemoveFieldUnsetFieldMessage
+        )
+      )
+
+    if (game.isMill(fields(0)))
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidRemoveFieldOnAMillMessage
+        )
+      )
+
+    Success(
+      nextState(
+        game.copy(
+          board = Board(
+            game.board.fields
+              .updated(
+                game.board.fields.indexOf(fields(0)),
+                fields(0).copy(color = fields(0).unsetFieldColor)
+              ),
+            game.board.size
+          )
+        ),
+        fields
+      )
+    )
+  }
+}
+case class FlyingState(
+    override val game: Game
+) extends GameState(game)
+    with Moving(game) {
+  override def nextState(
+      game: Game,
+      fields: (Field, Option[Field])
+  ): GameState = if (game.isMill(fields(1).get)) RemovingState(game)
+  else copy(game)
+  override def execute(fields: (Field, Option[Field])): Try[GameState] = {
+    if (!game.isValidSet(fields(1).get))
+      return Failure(
+        IllegalArgumentException(
+          Messages.invalidFlyingFieldAlreadyInUseMessage
+        )
+      )
+    movePiece(fields).map(game => nextState(game, fields))
+  }
+}
+
+enum GameEvent {
+  case OnSetting extends GameEvent
+  case OnMoving extends GameEvent
+  case OnRemoving extends GameEvent
+  case OnFlying extends GameEvent
 }
---
+++ src/main/scala/de/htwg/se/mill/util/Messages.scala
@@ -0,0 +1,40 @@
+package de.htwg.se.mill.util
+
+object Messages {
+  // GameState
+  val pieceNotMovedMessage = "The piece was not moved."
+  val pieceNotSetMessage = "The piece was not set."
+  val pieceNotRemovedMessage = "The piece was not removed."
+  val isNotSettingStateMessage =
+    s"$pieceNotSetMessage All pieces are already set."
+  val isNotMovingStateMessage =
+    s"$pieceNotMovedMessage Please provide a valid input for moving or flying a piece."
+  val isNotFlyingStateMessage =
+    s"$pieceNotMovedMessage Please provide a valid input for moving or flying a piece."
+  val isNotRemovingStateMessage =
+    s"$pieceNotRemovedMessage Please provide a valid input for removing a piece."
+  val movedOtherPieceMessage =
+    s"$pieceNotMovedMessage You can only move your own pieces."
+  val invalidMoveFieldAlreadyInUseMessage =
+    s"$pieceNotMovedMessage Please use a valid field that is not already in use."
+  val invalidSetFieldAlreadyInUseMessage =
+    s"$pieceNotSetMessage Please use a valid field that is not already in use."
+  val invalidRemoveFieldOwnPieceMessage =
+    s"$pieceNotRemovedMessage You cannot remove your own pieces."
+  val invalidRemoveFieldUnsetFieldMessage =
+    s"$pieceNotRemovedMessage You cannot remove unset fields."
+  val invalidRemoveFieldOnAMillMessage =
+    s"$pieceNotRemovedMessage You cannot remove pieces on a mill."
+  val invalidFlyingFieldAlreadyInUseMessage =
+    s"$pieceNotMovedMessage Please use a valid field that is not already in use."
+  // TUI
+  val wrongCommandMessage = "Your command is wrong. Please check it again. "
+  val wrongSettingOrRemovingCommandMessage =
+    wrongCommandMessage + "Should be something like 111 for removing or setting pieces."
+  val wrongMovingOrFlyingCommandMessage =
+    wrongCommandMessage + "Should be something like 111 121 for moving pieces."
+  val wrongFieldPositionMessage =
+    wrongCommandMessage + "The field position provided is invalid."
+  val wrongTargetFieldPositionMessage =
+    wrongCommandMessage + "The field position where the piece should be moved to is invalid."
+}
